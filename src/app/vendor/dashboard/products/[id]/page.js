e33c33c39f283549c1de170f289f669116dc5e6d
Aayush <ayushkarkee6@gmail.com>
2025-05-20 15:31:33 +0545

    completed product page

--- src/app/vendor/dashboard/products/[id]/page.js
+++ src/app/vendor/dashboard/products/[id]/page.js
@@ -1,5 +1,6 @@
 import { ProductDetails } from "@/components/vendor/product-details";
 
-export default function ProductDetailsPage({ params }) {
-  return <ProductDetails id={params.id} />;
+export default async function ProductDetailsPage({ params }) {
+  const { id } = await params; 
+  return <ProductDetails id={id} />;
 }
